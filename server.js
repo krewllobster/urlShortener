b9eea16f4b9d69a798cafb54c3a75cb6545e9127
Sebastian <krewllobster@gmail.com>
2017-08-11 14:48:41 -0400

    returning ip, language, and software from req.headers

--- server.js
+++ server.js
@@ -2,32 +2,15 @@ const express = require('express')
 const helmet = require('helmet')
 const url = require('url')
 const app = express()
-const retObj = {
-  unix: null,
-  natural: null,
-}
 
 app.use(helmet())
 
-app.route('/').get((req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html')
-})
-
 app.route(/\/(.*)/).get((req, res) => {
-  const path = decodeURIComponent(url.parse(req.url).path.slice(1))
-  var date
-  if (path.match(/^\d+$/)) {
-    console.log('path matched digits?')
-    date = new Date(parseInt(path, 10))
-  } else {
-    date = new Date(path)
-  }
-  const options = {month: 'long', day: 'numeric', year: 'numeric'}
-  if (Object.prototype.toString.call(date) === "[object Date]") {
-    retObj.natural = date.toLocaleString('en-US', options)
-    retObj.unix = Date.parse(date)
-  }
-  res.json(retObj)
+  res.json({
+    ipaddress: req.headers.host.toString(),
+    language: req.headers['accept-language'].split(',')[0],
+    software: req.headers['user-agent'].toString().match(/\(([^)]+)\)/)[1],
+  })
 })
 
 app.listen(process.env.PORT || 27010, () => {
